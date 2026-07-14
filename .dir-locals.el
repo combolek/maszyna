031a11cfa138435affb0ac52127e53130cd3dc6a
combolek <4743344+combolek@users.noreply.github.com>
2026-07-14 16:52:00 -0700

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((c-basic-offset . 4)
+         (indent-tabs-mode . t)
+         (tab-width . 4)) ))
